3d5486c21038a02e61acf9590959ef879d16494f
vpriyanshu671 <vpriyanshu671@gmail.com>
2024-06-23 21:03:23 +0530

    Feature : created urls.py file

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -22,6 +30,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [
