922f9c50a309c63cf2127a8f9efa9bfd240793b7
vpriyanshu671 <vpriyanshu671@gmail.com>
2024-07-12 21:46:28 +0530

    Feature : Add sending email part

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,45 @@
 from django.views.generic import ListView
 from .models import Post
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+                )
+            message = (f"Read {post.title} at {post_url}\n\n"
+                       f"{cd['name']}\'s comments:{cd['comments']}"
+                       )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
+                
+            
+            
 
 
 class PostListView(ListView):
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+# Email Server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_CONFIG = config('DEFAULT_FROM_EMAIL')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
